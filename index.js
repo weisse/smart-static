42161acf431396358ef502591d578e388efb8565
weisse <pietro.cucinella@kubique.com>
2016-02-09 09:34:06 +0100

    compression fallback implemented

--- index.js
+++ index.js
@@ -14,29 +14,29 @@ const mimeTypes = require("mime-types");
 const defaults = require(p.join(__dirname, "./defaults.json"));
 
 module.exports = function(path, options){
-	
+
 	if(!options) options = {};
 	options = _.extend(defaults, options);
-	
+
 	var cache = lru({
-		
+
 		length: function(element, key){
-			return element.data.length;			
+			return element.data.length;
 		},
 		max: options["max-cache-size"],
 		maxAge: options["max-cache-age"]
-	
+
 	});
-	
+
 	var basePath = p.resolve(process.cwd(), path);
-	
+
 	return function(req, res, next){
-		
+
 		var relativePath = req.path;
 		var absolutePath = p.join(basePath, relativePath);
-		
+
 		new bluebird.Promise(function(res, rej){
-			
+
 			/*
 			 * Get file stat object
 			 */
@@ -47,11 +47,11 @@ module.exports = function(path, options){
 					res(statObj);
 				}
 			});
-			
+
 		}).then(function(statObj){
-		
+
 			var fileObj = cache.get(absolutePath);
-			
+
 			/*
 			 * Check cache
 			 */
@@ -60,17 +60,18 @@ module.exports = function(path, options){
 					return fileObj;
 				}
 			}
-			
-			fileObj = {		
+
+			fileObj = {
 				stat: statObj,
-				contentType: mimeTypes.contentType(p.extname(absolutePath)) 
+				contentType: mimeTypes.contentType(p.extname(absolutePath)),
+				compressed: false
 			};
-			
+
 			/*
 			 * Read file
 			 */
 			return new bluebird.Promise(function(res, rej){
-				
+
 				fs.readFile(absolutePath, function(err, data){
 					if(err){
 						rej(err);
@@ -79,9 +80,9 @@ module.exports = function(path, options){
 						res(fileObj);
 					}
 				});
-				
+
 			}).then(function(fileObj){
-				
+
 				/*
 				 * Minify where possible and requested
 				 */
@@ -90,107 +91,97 @@ module.exports = function(path, options){
 				}else{
 					return fileObj;
 				}
-				
+
 			}).then(function(fileObj){
-				
+
 				/*
-				 * Compress using gzip or deflate
+				 * Compress using gzip
 				 */
 				if(options.compression && compressible(fileObj.contentType)){
-					var accept = accepts(req);
-					var accepted = accept.encoding(["gzip", "deflated"]);
-					if(accepted){
-						switch(accepted){
-							case "gzip":
-								fileObj.contentEncoding = "gzip";
-								var zipFn = zlib.gzip;
-							break;
-							case "deflate":
-								fileObj.contentEncoding = "deflate";
-								var zipFn = new zlib.deflate;
-							break;
-						}
-						if(zipFn){
-							return new bluebird.Promise(function(res, rej){
-								zipFn(fileObj.data, function(err, buffer){
-									if(err){
-										fileObj.compressed = false;
-										rej(err);
-									}else{
-										fileObj.compressed = true;
-										fileObj.data = buffer;
-										res(fileObj);
-									}
-								});
+						return new bluebird.Promise(function(res, rej){
+							zlib.gzip(fileObj.data, function(err, buffer){
+								if(err){
+									rej(err);
+								}else{
+									fileObj.compressed = true;
+									fileObj.data = buffer;
+									res(fileObj);
+								}
 							});
-						}
+						});
 					}
 				}
-				
-				fileObj.compressed = false;
+
 				return fileObj;
-				
+
 			}).then(function(fileObj){
-				
+
 				/*
 				 * Generate etag with provided algorithm
 				 */
 				if(options.etag){
 					fileObj.etag = crypto.createHash(options["hash-algorithm"]).update(fileObj.data).digest("hex");
 				}
-				
+
 				return fileObj;
-				
+
 			}).then(function(fileObj){
-				
+
 				/*
 				 * Write in cache
 				 */
 				cache.set(absolutePath, fileObj, options["max-cache-age"]);
 				return fileObj;
-				
+
 			});
-			
+
 		}).then(function(fileObj){
-			
-			var ifModifiedSince = req.get("if-modified-since");
-			var ifNoneMatch = req.get("if-none-match");
-			
-			if(options["if-modified-since"]){
-				if(ifModifiedSince == fileObj.stat.mtime.toString()){
-					res.status(304);
-					return;
-				}else{
-					res.setHeader("Last-Modified", fileObj.stat.mtime.toGMTString());
-				}
-			}
-			
-			if(options["etag"]){
-				if(ifNoneMatch == fileObj.etag){
-					res.status(304);
-					return;
-				}else{
-					res.setHeader("ETag", fileObj.etag);
-				}
-			}
-			
-			if(options["compression"] && fileObj.compressed){
-				res.setHeader("Content-Encoding", fileObj.contentEncoding);
-			}
-			
+
 			res.setHeader("Content-Type", fileObj.contentType);
-			res.status(200).write(fileObj.data);
-			
-		}).then(function(){
-			
-			if(options["continue"]){
-				next();
-			}else{
-				res.end();
+			res.setHeader("ETag", fileObj.etag);
+			res.setHeader("Last-Modified", fileObj.stat.mtime.toGMTString());
+
+			var ifModifiedSince = req.get("if-modified-since");
+
+			if(options["if-modified-since"] && ifModifiedSince === fileObj.stat.mtime.toString()){
+					res.status(304);
+					return;
 			}
-			
+
+			var ifNoneMatch = req.get("if-none-match");
+
+			if(options["etag"] && ifNoneMatch === fileObj.etag){
+					res.status(304);
+					return;
+			}
+
+			if(options["compression"] && fileObj.compressed){
+					var accept = accepts(req);
+					var accepted = accept.encoding(["gzip"]);
+					if(accepted){
+						res.setHeader("Content-Encoding", "gzip");
+						res.status(200).write(fileObj.data);
+					}else{
+						/*
+						 * Fallback for compatibility
+						 */
+						zlib.gunzip(fileObj.data, function(err, buffer){
+								if(err) throw new Error(err);
+								else res.status(200).write(buffer);
+						});
+					}
+				}
+			}else{
+				res.status(200).write(fileObj.data);
+			}
+
+		}).then(function(){
+
+			if(options["continue"]) next();
+			else res.end();
+
 		});
-		
+
 	};
-	
+
 };
